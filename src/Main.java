b7b38361e573c03aa7cf13583448068372c06181
krox <krox2005@yandex.ru>
2018-08-19 15:49:43 +0300

    Создан класс Main (пока пустой)

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/*
+Copyright (c) 2018 Petikov Software Development 
+Created by Petikov Vladimir on 19.08.18 15:21
+-----------------------------------
+Project    : ex180818
+
+*/
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
